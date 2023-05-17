7f36abeda26e6ac460883f865db3ae6c73fb245f
Arturo <jmbw.box@gmail.com>
2023-05-17 14:19:05 -0500

    added data_task module

---
+++ src/data_task.py
@@ -0,0 +1,28 @@
+from events import Events
+from log_listener import setup_log_event_handlers
+from task_listener import setup_task_event_handlers
+
+class DataTask:
+    
+    def __init__(self):
+        self.events = Events()
+        self.status_ok = True
+        setup_log_event_handlers(self.events)
+        setup_task_event_handlers(self.events)
+        
+    def start(self):
+        self.event_stack = {
+            'CONFIG': self.events.on_init_done,
+            'REQUEST': self.events.on_config_done,
+            'STRUCTURE': self.events.on_request_data_obtained,
+        }
+        
+        for event, function in self.event_stack.items():
+            
+            if not self.status_ok:
+                break
+            
+            function(self)
+            
+    
+    
