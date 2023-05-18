d6b8a731f4887a02b29d8adbc60afdd9ff19f5fe
Arturo <jmbw.box@gmail.com>
2023-05-18 12:59:35 -0500

    modified api client to store paginated data

--- src/api_client.py
+++ src/api_client.py
@@ -1,6 +1,7 @@
 import json
 from requests import get
-
+from time import sleep
+from rich import inspect
 class ApiClient:
     def __init__(self, data_task):
         self.parent_task = data_task
@@ -8,18 +9,25 @@ class ApiClient:
     def request_cards(self):
         API_ENDPOINT = 'https://api.scryfall.com/cards/search?q='
         request = API_ENDPOINT + self.parent_task.config.query_string
-        self.response = get(request)
-        self.response_data = self.response.json()
         
+        sleep(0.1)
+        self.response = get(request)
+        response_content = self.response.json()
         
         if not self.response.ok:
             self.parent_task.status_ok = False
             self.parent_task.events.on_bad_request(self.parent_task)
             return
         
-        if 'warnings' in self.response_data.keys():
-            self.response_warnings =  enumerate(self.response_data['warnings'])
+        if 'warnings' in response_content.keys():
+            self.response_warnings =  enumerate(response_content['warnings'])
             self.parent_task.events.on_warnings_found(self.parent_task)
+        
+        self.response_data = response_content['data']
+        
+        if response_content['has_more']:
+            next_pages = self.aggregate_paginated_data(response_content.next_page)
+            self.response_data += next_pages
             
         return
     
@@ -34,4 +42,13 @@ class ApiClient:
         return
         
         
-            
+    def aggregate_paginated_data(self, new_page_url):
+        
+        sleep(0.1)
+        new_page = get(new_page_url).json()
+        
+        if not new_page['has_more']:
+            return new_page['data']
+
+        next_page = self.aggregate_paginated_data(new_page['next_page'])
+        return new_page['data'] + next_page
--- src/task_listener.py
+++ src/task_listener.py
@@ -2,19 +2,23 @@ from task_config import TaskConfig
 from api_client import ApiClient
 
 def handle_tasks_on_init_done(data_task):
-    config = TaskConfig()        
+    config = TaskConfig()
+    setattr(data_task, 'config', config)
+    
     config.load_config()
     
-    setattr(data_task, 'config', config)
+    
 
 def handle_tasks_on_config_done(data_task):
     client = ApiClient(data_task)
+    setattr(data_task, 'client', client)
+    
     client.request_cards()
     
-    if client.parent_task.config.raw_dest_path:
+    if data_task.config.raw_dest_path:
         client.save_raw_response()
         
-    setattr(data_task, 'client', client)
+    
         
 # def handle_request_data_obtained(events, status_ok, config, client):
     
