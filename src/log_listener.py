f81fb6a0eb83b85e8d5c436ea59814f2b23a2c3f
Arturo <jmbw.box@gmail.com>
2023-05-18 11:25:14 -0500

    modified event implementation

--- src/log_listener.py
+++ src/log_listener.py
@@ -1,23 +1,23 @@
 from loguru import logger
 
-def handle_on_config_done(data_task):
+def handle_log_on_config_done(data_task):
     logger.info(f'\n\tPerforming query for {data_task.config.query_string}, the results will be stored in {data_task.config.processed_dest_path}')
         
     if data_task.config.raw_dest_path:
         logger.info(f"\n\tA copy of the raw query response will be stored in {data_task.config.raw_dest_path.resolve()}")   
 
-def handle_bad_request(client):
-    logger.error(f"\n\tRequest failed. The following details were given by the API \"{client.response_data['details']}\".")
+def handle_log_on_bad_request(data_task):
+    logger.error(f"\n\tRequest failed. The following details were given by the API \"{data_task.client.response_data['details']}\".")
 
-def handle_warnings_found(client):
-    for index, warning in client.response_warnings:
+def handle_log_on_warnings_found(data_task):
+    for index, warning in data_task.client.response_warnings:
         logger.warning(f'\n\t{index + 1}: {warning}')
         
-def handle_raw_save_failed(client):
-    logger.error(f'\n\tCould not write a file to store the raw response data on given path {client.raw_dest_path}')
+def handle_log_on_raw_save_failed(data_task):
+    logger.error(f'\n\tCould not write a file to store the raw response data on given path {data_task.config.raw_dest_path}')
     
 def setup_log_event_handlers(events):
-    events.on_config_done += handle_on_config_done
-    events.on_bad_request += handle_bad_request
-    events.on_warnings_found += handle_warnings_found
-    events.on_raw_save_failed += handle_raw_save_failed
+    events.on_config_done += handle_log_on_config_done
+    events.on_bad_request += handle_log_on_bad_request
+    events.on_warnings_found += handle_log_on_warnings_found
+    events.on_raw_save_failed += handle_log_on_raw_save_failed
--- src/task_listener.py
+++ src/task_listener.py
@@ -1,17 +1,17 @@
 from task_config import TaskConfig
 from api_client import ApiClient
 
-def handle_on_init_done(data_task):
+def handle_tasks_on_init_done(data_task):
     config = TaskConfig()        
     config.load_config()
     
     setattr(data_task, 'config', config)
 
-def handle_on_config_done(data_task):
+def handle_tasks_on_config_done(data_task):
     client = ApiClient(data_task)
     client.request_cards()
     
-    if client.raw_dest_path:
+    if client.parent_task.config.raw_dest_path:
         client.save_raw_response()
         
     setattr(data_task, 'client', client)
@@ -20,5 +20,5 @@ def handle_on_config_done(data_task):
     
     
 def setup_task_event_handlers(events):
-    events.on_init_done += handle_on_init_done
-    events.on_config_done += handle_on_config_done
+    events.on_init_done += handle_tasks_on_init_done
+    events.on_config_done += handle_tasks_on_config_done
